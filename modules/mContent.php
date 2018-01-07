acb6c53c1684fbfeddda6ae4c655e188503fc191
Pham Kieu Tan <1560284@student.hcmus.edu.vn>
2018-01-07 14:33:11 +0700

    update mContent.php case a

--- modules/mContent.php
+++ modules/mContent.php
@@ -5,32 +5,30 @@
         case 1:
             include ("pages/pHome.php");
             break;
-        // case 2: 
-        //     include ("pages/pSanPhamTheoHang.php");
-        //     break;
-        // case 3: 
-        //     include ("pages/pSanPhamTheoLoai.php");
-        //     break;
-        // case 4:
-        //     include ("pages/pChiTietSanPham.php");
-        //     break;
-        // case 6:
-        //     include ("pages/pQuanLyGioHang.php");
-        //     break;
-        // case 10:
-        //     include ("pages/exDangNhap.php");
-        //     break;
-        // case 11:
-        //     include ("pages/exDangXuat.php");
-        //     break;
-        // case 12:
-        //     include ("pages/exThemSanPhamVaoGioHang.php");
-        //     break;
-        // case 13:
-        //     include ("pages/exCapNhatGioHang.php");
-        //     break;
-        // default:
-        //     include "pages/pError.php";
-        //     break;
+        case 2: 
+            include ("pages/pSanPhamTheoHang.php");
+            break;
+        case 3: 
+            include ("pages/pSanPhamTheoLoai.php");
+            break;
+        case 4:
+            include ("pages/pThongTinChiTietSanPham.php");
+            break;
+        case 5:
+            include ("pages/pTimKiem.php");
+            break;
+        case 6:
+            include ("pages/pXuLyDangNhap.php");
+            break;
+        case 7:
+            include ("pages/pDangKy.php");
+            break;
+        case 8:
+        include ("admin/index.php");
+        break;
+        
+        default:
+            include ("pages/pError.php");
+            break;
     }
 ?>
