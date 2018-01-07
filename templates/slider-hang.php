d1708b058eb078dd48ddbbcf6bed168a3dc4dacd
Pham Kieu Tan <1560284@student.hcmus.edu.vn>
2018-01-07 10:05:52 +0700

    add slider and ThongTinSP

---
+++ templates/slider-hang.php
@@ -0,0 +1,21 @@
+<div class="bottom-content">
+    <div class="slider-hang">
+        <marquee id="slide-hang" style="height: 61px;">
+            <div onMouseOver="document.getElementById('slide-hang').stop();" onMouseOut="document.getElementById('slide-hang').start();">
+                <img src="./logo-hang/brand-kingmax.jpg" alt="">
+                <img src="./logo-hang/brand-apple.jpg" alt="">
+                <img src="./logo-hang/brand-dell.jpg" alt="">
+                <img src="./logo-hang/brand-intel.jpg" alt="">
+                <img src="./logo-hang/brand-hp.jpg" alt="">
+                <img src="./logo-hang/brand-samsung.jpg" alt="">
+                <img src="./logo-hang/brand-sony.jpg" alt="">
+                <img src="./logo-hang/brand-sandisk.png" alt="">
+                <img src="./logo-hang/brand-logitech.jpg" alt="">
+                <img src="./logo-hang/brand-transcend.png" alt="">
+                <img src="./logo-hang/brand-lenovo.jpg" alt="">
+                <img src="./logo-hang/brand-asus.jpg" alt="">
+                <img src="./logo-hang/brand-wd.png" alt="">
+            </div>
+        </marquee>
+    </div>
+</div>
---
+++ templates/tempThongTinSanPham.php
@@ -0,0 +1,12 @@
+<div class="box">
+    <div class="img-respponsive">
+        <a href="index.php?a=4&id=<?php echo $maSanPham; ?>"><img src="images/<?php echo $hinhURL; ?>"/></a>  
+    </div>
+    <div class="box-info">
+        <div class="TenSanPham"><?php echo $tenSanPham; ?></div>
+        <div class="GiaSanPham">Giá: <?php echo $giaSanPham; ?> VNĐ</div>
+        <div class="ChiTietSanPham">
+            <a href="index.php?a=4&id=<?php echo $maSanPham; ?>"> Chi Tiết </a> 
+        </div>
+    </div>
+</div>
