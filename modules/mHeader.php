1bdd9545037a107a8a48e5391abe269643bc0337
Pham Kieu Tan <1560284@student.hcmus.edu.vn>
2018-01-07 14:35:10 +0700

    update login logout

--- modules/mHeader.php
+++ modules/mHeader.php
@@ -2,12 +2,11 @@
     <div class="top-header">
         <div id="topright-header">
             <ul>
-                <li><a href="#"><span><img src="./icon/Add.png" alt="" title="Đăng Ký Thành Viên"</span>Đăng Ký</a></li>
-                <li><a href="#"><span><img src="./icon/User-Login-24.png" alt=""  title="Đăng Nhập mua hàng"></span>Đăng Nhập</a></li>
+                <li><a href="index.php?a=7"><span><img src="./icon/Add.png" alt="" title="Đăng Ký Thành Viên"</span>Đăng Ký</a></li>
+                <li><a href="index.php?a=6"><span><img src="./icon/User-Login-24.png" alt=""  title="Đăng Nhập mua hàng"></span>Đăng Nhập</a></li>
                 <li><a href="#">Kiểm Tra Đơn Hàng</a></li>
                 <li><span><a href="#">Khuyến mãi HOT</a></span></li>
-                <li><span class="nokichuong"><img src="./icon/icon-chuong.png" alt="" title="Khuyến mãi HOT"></span></li>
-                
+                <li><span class="nokichuong"><img src="./icon/icon-chuong.png" alt="" title="Khuyến mãi HOT"></span></li>              
             </ul>    
         </div>   
         <div id="topleft-header">
@@ -20,18 +19,24 @@
     <div class="bottom-header">
         <div id="bottomleft-header">
             <!-- <a><b><i>thegioilinhkiendientu.com</i></b></a> -->
-            <img src="./icon/logoshop.PNG" alt="" title="thegioilinhkiendientu.com, Xin chào !">
+           <a href="index.php"><img src="./icon/logoshop.PNG" alt="" title="thegioilinhkiendientu.com, Xin chào !"></a>
         </div>
         <div class="bottom-center-header">
-            <div id="input-search">
-                <select name="txtTimKiem" id="select-search">
-                    <option value="0">Tất cả danh mục</option>
-                    <option value="1">Mouse ko dây</option>
-                    <option value="2">Mouse có dây</option>
-                </select>
-                <input type="search" id="search" placeholder="Bạn cần tìm gì hôm nay?" />
-                <button class="icon"><a href="#"><i class="fa fa-search"></a></i></button>
-            </div>
+            <form name="TimKiem" action="index.php?a=5" method="post">
+                <div id="input-search">
+                    <select name="TuyChon" id="select-search">
+                        <option value="0">Tất cả danh mục</option>
+                        <option value="1">Hãng Sản Xuất</option>
+                        <option value="2">Loại Sản Phẩm</option>
+                        <option value="3">Giá Thấp Dần</option>
+                        <option value="4">Giá Tăng Dần</option>
+                    </select>
+                    <input type="search" name="TuKhoa" id="search" placeholder="Bạn cần tìm gì hôm nay?" />
+                    <button class="icon" name="TimKiem" type="submit"/>
+						<i class="fa fa-search"></i>
+                    </button>
+                </div>
+            </form>
             <div id="bottom-right-header">
                 <ul>
                     <li>(<span>0</span>)<a href="#"><span><img src="./icon/bag.png" width="30" height="25"alt="" title="Giỏ hàng của bạn"></span>Giỏ hàng</a></li>  
