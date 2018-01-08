53b34c72339a24cdce7a216302c612cb93e103a8
Pham Kieu Tan <1560284@student.hcmus.edu.vn>
2018-01-08 12:27:35 +0700

    update mHeader - log in log out

--- modules/mHeader.php
+++ modules/mHeader.php
@@ -1,47 +1,20 @@
-<header class="header">
-    <div class="top-header">
-        <div id="topright-header">
-            <ul>
-                <li><a href="index.php?a=7"><span><img src="./icon/Add.png" alt="" title="Đăng Ký Thành Viên"</span>Đăng Ký</a></li>
-                <li><a href="index.php?a=6"><span><img src="./icon/User-Login-24.png" alt=""  title="Đăng Nhập mua hàng"></span>Đăng Nhập</a></li>
-                <li><a href="#">Kiểm Tra Đơn Hàng</a></li>
-                <li><span><a href="#">Khuyến mãi HOT</a></span></li>
-                <li><span class="nokichuong"><img src="./icon/icon-chuong.png" alt="" title="Khuyến mãi HOT"></span></li>              
-            </ul>    
-        </div>   
-        <div id="topleft-header">
-            <ul>
-                <li><a href="index.php"><span><img src="./icon/Home.png" alt=""  title="Trang chủ"></span></a></li>
-                <li id="Hotline">Hotline: <span><img src="./icon/tel.png" width="20" height="20" alt=""  title="Hotline"></span> 01234 567 890 </li>
-            </ul>    
-        </div>  
+<div id="header">
+    <a href="index.php">
+        <img src="img/logo.gif" width="519" height="63">
+    </a>
+    <div id="login_nav">
+        <?php
+            if(isset($_SESSION['maTaiKhoan']))
+            {
+                include ("modules/mThongTinTaiKhoan.php");
+            }
+            else
+            {
+                include ("modules/mDangNhapTaiKhoan.php");
+            }
+        ?>
+        
     </div>
-    <div class="bottom-header">
-        <div id="bottomleft-header">
-            <!-- <a><b><i>thegioilinhkiendientu.com</i></b></a> -->
-           <a href="index.php"><img src="./icon/logoshop.PNG" alt="" title="thegioilinhkiendientu.com, Xin chào !"></a>
-        </div>
-        <div class="bottom-center-header">
-            <form name="TimKiem" action="index.php?a=5" method="post">
-                <div id="input-search">
-                    <select name="TuyChon" id="select-search">
-                        <option value="0">Tất cả danh mục</option>
-                        <option value="1">Hãng Sản Xuất</option>
-                        <option value="2">Loại Sản Phẩm</option>
-                        <option value="3">Giá Thấp Dần</option>
-                        <option value="4">Giá Tăng Dần</option>
-                    </select>
-                    <input type="search" name="TuKhoa" id="search" placeholder="Bạn cần tìm gì hôm nay?" />
-                    <button class="icon" name="TimKiem" type="submit"/>
-						<i class="fa fa-search"></i>
-                    </button>
-                </div>
-            </form>
-            <div id="bottom-right-header">
-                <ul>
-                    <li>(<span>0</span>)<a href="#"><span><img src="./icon/bag.png" width="30" height="25"alt="" title="Giỏ hàng của bạn"></span>Giỏ hàng</a></li>  
-                </ul>
-            </div>
-        </div>
-    </div>
-</header>
+    <img src="img/header_1.png" width="748">
+    <img src="img/header_2.png" width="748">
+</div>
