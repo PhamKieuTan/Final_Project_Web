60a623c221c2c6c3fac5bbcbe9f7495441a4991a
Pham Kieu Tan <1560284@student.hcmus.edu.vn>
2018-01-08 12:42:06 +0700

    add xu ly dang nhap

---
+++ pages/pXuLyDangNhap.php
@@ -0,0 +1,33 @@
+<?php
+    //Xử lý đăng nhập
+    if(isset($_POST['txtUs']) && isset($_POST['txtPs']))
+    {
+        $User = $_POST['txtUs'];
+        $Pas = $_POST['txtPs'];
+
+        $sql = "SELECT MaTaiKhoan,MaLoaiTaiKhoan,TenDangNhap,TenHienThi,MatKhau FROM TaiKhoan 
+                WHERE BiXoa = 0 AND TenDangNhap = '$User' AND MatKhau ='$Pas'";
+        $result = DataProvider::ExecuteQuery($sql);
+        $row = mysqli_fetch_array($result);
+        if($row != null)
+        {
+            $_SESSION["maTaiKhoan"] = $row['MaTaiKhoan'];
+            $_SESSION["maLoaiTaiKhoan"] = $row['MaLoaiTaiKhoan'];
+            $_SESSION["tenHienThi"] = $row['TenHienThi'];          
+            if($_SESSION["maLoaiTaiKhoan"] == 4)
+                {
+                    DataProvider::ChangeURL("admin/index.php");
+                } 
+            else    
+            {
+                echo "<script>alert('Đăng nhập thành công !');</script>";
+                DataProvider::ChangeURL("index.php?a=11");
+            }
+        }
+        else
+        {
+            echo "<script>alert('Sai tên đăng nhập hoặc mật khẩu !');</script>";
+            DataProvider::ChangeURL("index.php?a=6");
+        }
+    }
+?>
