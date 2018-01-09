96b35d9d442d88ee66c194c9fdec496d3c16eaaa
tnghia97 <ellken97@gmail.com>
2018-01-10 02:09:28 +0700

    upload file tLoaiSanPham

---
+++ admin/tLoaiSanPham.php
@@ -0,0 +1,7 @@
+<div>
+    <tr>
+        <td align="center"> <?php echo $i++; ?></td>
+        <td align="center"> <?php echo $mahsx; ?></td>
+        <td align="center"> <?php echo $tenhsx; ?></td>
+    </tr>
+</div>
