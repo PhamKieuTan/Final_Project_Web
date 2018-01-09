f5f513b2aab19a3ebb6a8058ba43da107564ac62
HiepLinh97 <hanamisaomchi001@gmail.com>
2018-01-09 14:59:24 +0700

    update file XuLyDangKy

--- pages/pXuLyDangKy.php
+++ pages/pXuLyDangKy.php
@@ -7,12 +7,44 @@
         $email = $_POST['txtEmail'];
         $tel = $_POST['txttel'];
         $DiaChi = $_POST['city'];
-
-        $sql = "INSERT INTO TAIKHOAN (TenDangNhap,MatKhau,TenHienThi,DiaChi,email,DienThoai)
-                 VALUES ('$Username','$Password','$HoTen','$DiaChi','$email','$tel')";
-
-        $result = DataProvider::ExecuteQuery($sql);
-        
-        DataProvider::ChangeURL("index.php?a=6");
+        if (!$Username || !$Password || !$email || !$HoTen)
+        {
+            echo "Tên đăng nhập, mật khẩu, email và họ tên không được trống. <a href='javascript: history.go(-1)'>Trở lại</a>";
+            exit;
+        }
+        $Password = md5($Password);
+        if (mysql_num_rows(mysql_query("SELECT TenDangNhap FROM TaiKhoan WHERE TenDangNhap = '$Username'")) > 0)
+        {
+            echo "Tên đăng nhập này đã có người dùng. Vui lòng chọn tên đăng nhập khác.";
+            DataProvider::ChangeURL("index.php?a=8");
+            exit;
+        }
+        if (!eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$", $email))
+        {
+            echo "Email này không hợp lệ. Vui long nhập email khác.";
+            DataProvider::ChangeURL("index.php?a=8");
+            exit;
+        }
+        if (mysql_num_rows(mysql_query("SELECT email FROM TaiKhoan WHERE email='$email'")) > 0)
+        {
+            echo "Email này đã có người dùng. Vui lòng chọn Email khác.";
+            DataProvider::ChangeURL("index.php?a=8");
+            exit;
+        }
+        @$add = mysql_query("INSERT INTO TaiKhoan (
+            TenDangNhap,
+            MatKhau,
+            TenHienThi,
+            DiaChi,
+            DienThoai,
+            Emails
+        )
+        VALUE (
+            '{$Username}',
+            '{$Password}',
+            '{$HoTen}',
+            '{$DiaChi}',
+            '{$tel}',
+            '{$email}'")
     }
 ?>
