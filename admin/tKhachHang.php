fcd91dac04234406e393120cc720251d4e479dcf
tnghia97 <ellken97@gmail.com>
2018-01-10 02:05:56 +0700

    upload file tKhachHang

---
+++ admin/tKhachHang.php
@@ -0,0 +1,9 @@
+<div>
+    <tr>
+        <td align="center"> <?php echo $i++; ?></td>
+        <td align="center"> <?php echo $Ten; ?></td>
+        <td align="center"> <?php echo $Diachi; ?></td>
+        <td align="center"> <?php echo $SDT; ?></td>
+        <td align="center"> <?php echo $email; ?></td>
+    </tr>
+</div>
