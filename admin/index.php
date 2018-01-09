2c1681d75a337ba2fd8822370b6efe750b690dc6
Pham Kieu Tan <1560284@student.hcmus.edu.vn>
2018-01-10 01:41:20 +0700

    update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,7 @@
 <?php 
 	session_start();
-	// include_once ('lib/DataProvider.php'); 
-	// include_once ('lib/thegioilinhkiendientu.php');
+	include_once ('./lib/DataProvider.php'); 
+	include_once ('./lib/thegioilinhkiendientu.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,22 +19,34 @@
             <h1> Admin Shop</h1>
         </div>
         <div class="header-right">
-          
             <ul class="menu-admin">
-                <li class="icon-right" id="menu-shop"><a href="#">Hãng Sản Xuất</a></li>  
-                <li class="icon-right" id="menu-shop"><a href="#">Loại Sản Phẩm</a></li>
-                <li class="icon-right" id="menu-shop"><a href="#">Sản Phẩm</a></li>  
-                <li class="icon-right" id="menu-shop"><a href="#">Đơn Đặt Hàng</a></li>
-                <li class="icon-right" id="menu-shop"><a href="#">Khách Hàng</a></li>  
+                <li class="icon-right" id="menu-shop">
+                    <a href="index.php?a=1"><span><img src="../icon/Home.png" alt=""></span>Trang Chủ</a>
+                </li>
+                <li class="icon-right" id="menu-shop">
+                    <a href="index.php?a=2"><span><img src="../icon/newspaper.png" alt=""></span>Hãng Sản Xuất </a>
+                </li>
+                <li class="icon-right" id="menu-shop">
+                    <a href="index.php?a=3"><span><img src="../icon/newspaper.png" alt=""></span>Loại Sản Phẩm</a>
+                </li>
+                <li class="icon-right" id="menu-shop">
+                    <a href="index.php?a=4"><span><img src="../icon/newspaper.png" alt=""></span>Sản Phẩm</a>
+                </li>
+                <li class="icon-right" id="menu-shop">
+                    <a href="index.php?a=5"><span><img src="../icon/newspaper.png" alt=""></span>Đơn Đặt Hàng</a>
+                </li>
+                <li class="icon-right" id="menu-shop">
+                    <a href="index.php?a=6"><span><img src="../icon/user1.png" alt=""></span>Khách Hàng</a>
+                </li>           
             </ul>
-    
         </div>
     </div>
     <div class="wrapper">
-       
         <div class="content">
-
-        <div>
+            <?php
+                include ('mContent.php');
+            ?>
+        </div>
         <div class="footer">
             <div class"footer-bottom">
                 <hr boder="2">
