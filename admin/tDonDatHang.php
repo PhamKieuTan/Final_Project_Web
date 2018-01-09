a823d1a4492424b7f93c21069b5687ba785ad5b0
Pham Kieu Tan <1560284@student.hcmus.edu.vn>
2018-01-10 01:44:51 +0700

    add tDonDatHang

---
+++ admin/tDonDatHang.php
@@ -0,0 +1,8 @@
+<div>
+    <tr>
+        <td align="center"> <?php echo $i++; ?></td>
+        <td align="center"> <?php echo $mahd; ?></td>
+        <td align="center"> <?php echo $ngay; ?></td>
+        <td align="center"> <?php echo $tien; ?></td>
+    </tr>
+</div>
