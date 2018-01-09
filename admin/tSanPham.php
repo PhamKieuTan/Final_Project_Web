578864915f893708a20f24b8c12679bbd9ba7950
tnghia97 <ellken97@gmail.com>
2018-01-10 02:01:54 +0700

    upload file tSanPham

---
+++ admin/tSanPham.php
@@ -0,0 +1,12 @@
+<div>
+    <tr>
+        <td align="center"> <?php echo $i++; ?></td>
+        <td align="center"> <?php echo $maSP; ?></td>
+        <td align="center"> <?php echo $tenSanPham; ?></td>
+        <td align="center"><img src="../images/<?php echo $hinhURL; ?>" width="150" height="70" /></td>
+        <td align="center"> <?php echo $giaSanPham; ?></td>
+        <td align="center"> <?php echo $soLuongBan; ?></td>
+        <td align="center"> <?php echo $soLuongTon; ?></td>
+        <td align="center"> <?php echo $soLuotXem; ?></td>
+    </tr>
+</div>
