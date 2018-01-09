cd1c8f747ae02a7370e781f8954073ea0cafd4da
HiepLinh97 <hanamisaomchi001@gmail.com>
2018-01-09 22:44:56 +0700

    update file index.php of admin

--- admin/index.php
+++ admin/index.php
@@ -13,4 +13,34 @@
     <link rel="stylesheet" href="css/style.css" type="text/css">
     <title></title>
 </head>
+<body>
+    <div class="header">
+        <div class="header-left">
+            <h1> Admin Shop</h1>
+        </div>
+        <div class="header-right">
+          
+            <ul class="menu-admin">
+                <li class="icon-right" id="menu-shop"><a href="#">Hãng Sản Xuất</a></li>  
+                <li class="icon-right" id="menu-shop"><a href="#">Loại Sản Phẩm</a></li>
+                <li class="icon-right" id="menu-shop"><a href="#">Sản Phẩm</a></li>  
+                <li class="icon-right" id="menu-shop"><a href="#">Đơn Đặt Hàng</a></li>
+                <li class="icon-right" id="menu-shop"><a href="#">Khách Hàng</a></li>  
+            </ul>
+    
+        </div>
+    </div>
+    <div class="wrapper">
+       
+        <div class="content">
+
+        <div>
+        <div class="footer">
+            <div class"footer-bottom">
+                <hr boder="2">
+                <p class="copyright">All Rights Reserved.Design by Student University of Science @2017.</p> 
+            </div>
+        </div>
+    </div>
+</body>
 </html>
